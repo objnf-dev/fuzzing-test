a51e3fa86173058c5394e68666e77274c608b647
ObjectNotFound <xml@live.com>
2019-10-27 00:59:27 +0800

    Signed-off-by: ObjectNotFound <xml@live.com>

--- ModBusSlave/slave_v6.c
+++ ModBusSlave/slave_v6.c
@@ -11,22 +11,22 @@
 int main(void) {
 	// Variable
 	modbus_t *slave = NULL;
-	modbus_mapping_t *localdata = NULL;
+	modbus_mapping_t *localData = NULL;
 	int s = -1;
 
-	// Initalize local data section
-	localdata = modbus_mapping_new(MODBUS_MAX_READ_BITS, MODBUS_MAX_WRITE_BITS, MODBUS_MAX_READ_REGISTERS, MODBUS_MAX_WRITE_REGISTERS);
-	if(localdata == NULL) {
-		printf("Error occoured when mapping data section, Message: %s", modbus_strerror(errno));
+	// Initialize local data section
+	localData = modbus_mapping_new(MODBUS_MAX_READ_BITS, MODBUS_MAX_WRITE_BITS, MODBUS_MAX_READ_REGISTERS, MODBUS_MAX_WRITE_REGISTERS);
+	if(localData == NULL) {
+		printf("Error occurred when mapping data section, Message: %s", modbus_strerror(errno));
 		return -1;
 	}
 
-	// Create connection, Listening on all IPv6 addr
-	slave = modbus_new_tcp_pi("::0", "503");
+	// Create connection, Listening on all IPv4 address
+	slave = modbus_new_tcp_pi("::0", "502");
 	s = modbus_tcp_pi_listen(slave, 1);
 	modbus_tcp_pi_accept(slave, &s);
 	if(s == -1) {
-		printf("Error occoured when creating socket. Message: %s", modbus_strerror(errno));
+		printf("Error occurred when creating socket. Message: %s", modbus_strerror(errno));
 		return -1;
 	}
 
@@ -34,26 +34,22 @@ int main(void) {
 	modbus_set_debug(slave, TRUE);
 
 	// Space for master query
-	while(TRUE) {
-		uint8_t query[MODBUS_MAX_ADU_LENGTH];
-		memset(query, 0, MODBUS_MAX_ADU_LENGTH);
+	uint8_t query[MODBUS_MAX_ADU_LENGTH];
+	memset(query, 0, MODBUS_MAX_ADU_LENGTH);
 
-		// Try to receive query
-		int rc = modbus_receive(slave, query);
-		if(rc == -1) {
-			printf("Message error or connection closed. Message: %s", modbus_strerror(errno));
-			break;
-		}
-
-		// Reply query
-		int rec = modbus_reply(slave, query, rc, localdata);
-		if(rec == -1)
+	// Try to receive query
+	int rc = -1;
+	rc = modbus_receive(slave, query);
+	if(rc > 0) {
+		const int returnCode = modbus_reply(slave, query, rc, localData);
+		if(returnCode == -1)
 			printf("Failed to send reply. Message: %s", modbus_strerror(errno));
-	}
+	} else if(rc == -1)
+		printf("Connection maybe closed. Message: %s", modbus_strerror(errno));
 
 	// Close Server
 	close(s);
-	modbus_mapping_free(localdata);
+	modbus_mapping_free(localData);
 	modbus_close(slave);
 	modbus_free(slave);
 	return 0;
